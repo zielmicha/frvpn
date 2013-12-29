f1e52a7092e58f07ef0eb62b11b8f6babe4f6ee8
Michał Zieliński <michal+asus@zielinscy.org.pl>
2013-12-29 16:03:16 +0100

    improve link.py

--- link.py
+++ link.py
@@ -3,6 +3,9 @@ from blist import blist
 import time
 import struct
 import collections
+import logging
+
+logger = logging.getLogger('link')
 
 KB = 1000
 MB = 1000 * KB
@@ -21,7 +24,7 @@ STAT_AVG_COUNT = 4
 
 _Stat = collections.namedtuple('_Stat', 'num ping quality')
 
-class _LinkSender:
+class _LinkSend:
     '''
     Implements sending side of link.
     '''
@@ -40,6 +43,7 @@ class _LinkSender:
         self.last_packets = blist()
         self._used_bandwidth = 0
         self._stats = blist()
+        self._last_req = 0
 
     def get_used_bandwidth(self):
         # Amortized time O(log n)
@@ -62,7 +66,12 @@ class _LinkSender:
         self.impl.send(header + data)
         self._recalc()
 
-    def _send_req(self):
+    def maybe_send_req(self):
+        if time.time() - STAT_INTERVAL > self._last_req:
+            self.send_req()
+
+    def send_req(self):
+        self._last_req = time.time()
         pack = PACKET_STAT_REQ
         pack += struct.pack('!QdQQ', self.stat_num, time.time(),
                             self.packet_num, self.last_packet_num)
@@ -73,7 +82,7 @@ class _LinkSender:
     def _recv_stat(self, data):
         stat_num, quality, send_time = struct.unpack('!xQdd', data)
         recv_time = time.time()
-        ping = send_time - recv_time
+        ping = recv_time - send_time
         self._stats.append(_Stat(stat_num, ping, quality))
         self._recalc()
 
@@ -117,12 +126,13 @@ class _LinkSender:
         # stay with too large calc_bandwidth.
         # If link quality has not decreased _recalc_bandwidth
         # will increase it back.
-        t = self._last_bandwidth_scale - time.time()
+        t = time.time() - self._last_bandwidth_scale
         count = int(t // BANDWIDTH_SCALE_INTERVAL)
         self._last_bandwidth_scale += count * BANDWIDTH_SCALE_INTERVAL
         self.calc_bandwidth *= (BANDWIDTH_SCALE ** count)
 
 def avg(s):
+    s = list(s)
     return sum(s) / len(s)
 
 class _LinkRecv:
@@ -134,7 +144,7 @@ class _LinkRecv:
 
     def _recv_req(self, data):
         stat_num, send_time, \
-            end_num, start_num = struct.pack('!xQdQQ', data)
+            end_num, start_num = struct.unpack('!xQdQQ', data)
         quality = self._calc_quality(start_num, end_num)
         self._send_stat(stat_num, quality, send_time)
 
@@ -145,7 +155,7 @@ class _LinkRecv:
             return 1.
         else:
             success = 0
-            for i in packet_nums:
+            for i in self.packet_nums:
                 if start <= i <= end:
                     success += 1
             self.packet_nums = []
@@ -154,10 +164,69 @@ class _LinkRecv:
     def _send_stat(self, stat_num, quality, send_time):
         packet = PACKET_STAT
         packet += struct.pack('!Qdd', stat_num, quality, send_time)
-        self.send(packet)
+        self.impl.send(packet)
 
     def _recv_packet(self, data):
         packet_num, = struct.unpack('!Q', data[1:9])
         body = data[9:]
-
+        self.packet_nums.append(packet_num)
         self.recv_callback(body)
+
+class Link:
+    def __init__(self, impl, recv_callback):
+        self.linkrecv = _LinkRecv(impl, recv_callback)
+        self.linksend = _LinkSend(impl)
+        self.impl.recv_callback = self._recv
+
+    def _recv(self, data):
+        type = data[0]
+        if type == PACKET_STAT:
+            self.linksend._recv_stat(data)
+        elif type == PACKET_STAT_REQ:
+            self.linkrecv._recv_req(data)
+        elif type == PACKET_DATA:
+            self.linkrecv._recv_packet(data)
+        else:
+            logger.error('Invalid package type')
+
+    def __getattr__(self, name):
+        if hasattr(self.linkrecv, name):
+            return getattr(self.linkrecv, name)
+        else:
+            return getattr(self.linksend, name)
+
+    def __repr__(self):
+        return '<Link 0x%x bw=%d KBps ping=%d ms>' % (
+            id(self), self.calc_bandwidth / KB,
+            self.calc_ping * 1000)
+
+def create_link_pair():
+    a = NullLinkImpl()
+    b = NullLinkImpl()
+    a.other = b
+    b.other = a
+    return a, b
+
+class NullLinkImpl:
+    # default
+    recv_callback = lambda x: None
+    def send(self, data):
+        #print hex(id(self)), 'send', repr(data)
+        self.other.recv_callback(data)
+
+if __name__ == '__main__':
+    def logger(s):
+        pass #print 'recv', repr(s)
+
+    a, b = create_link_pair()
+
+    linka = Link(a, logger)
+    linkb = Link(b, logger)
+
+    for i in xrange(50000):
+        if i % 1000 == 0:
+            print i
+        linka.send('boo' * 200)
+        linka.maybe_send_req()
+
+    print linka
